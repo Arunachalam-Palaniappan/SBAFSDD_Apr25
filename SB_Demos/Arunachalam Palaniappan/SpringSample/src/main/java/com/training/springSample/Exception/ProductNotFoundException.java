bce7af2a5bfd8a26fbb49895ea124dc415a19dff
Arunachalam-Palaniappan <arunachalamp@fsstech.com>
2025-04-14 10:41:12 +0530

    Delete SB_Demos/Arunachalam Palaniappan/SpringSample directory

--- SB_Demos/Arunachalam Palaniappan/SpringSample/src/main/java/com/training/springSample/Exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.training.springSample.Exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(int id) {
-        super("Product with id " + id + " not found");
-    }
-}
--- SB_Demos/Arunachalam Palaniappan/SpringSample/src/test/java/com/training/springSample/stubRepository.java
+++
@@ -1,7 +0,0 @@
-package com.training.springSample;
-
-import com.training.springSample.Model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface stubRepository extends CrudRepository<Product, Integer> {
-}
